059dda504616c2ca798d99278250767a4400d9ab
Suchetaaa <sucheta317@gmail.com>
2019-11-01 20:04:31 +0530

    main stuff done

--- Assignment-4/windy_gridworld.py
+++ Assignment-4/windy_gridworld.py
@@ -2,36 +2,63 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import random
 
-# actions: 
-# 0 : up 
-# 1 : down
-# 2 : right 
-# 3 : left
-
-def nextstate_reward(num_rows, num_columns, wind_dist, start_block, goal_block, present_state, action):
+def nextstate_reward(num_rows, num_columns, wind_dist, start_block, goal_block, present_state, action, stochastic, seed):
+	# np.random.seed(seed)
 	reward = -1
-	wind_strength = wind_dist[int(present_state[1])]
+	if stochastic == 0:
+		wind_strength = wind_dist[int(present_state[1])]
+	else: 
+		wind_strength = wind_dist[int(present_state[1])] + random.randint(-1, 1)
+
 	next_state = np.asarray([0, 0], dtype=int)
 
+	# up
 	if action == 0:
-		next_state[0] = max(present_state[0] - 1 - wind_strength, 0)
-		next_state[1] = max(present_state[1], 0)
+		next_state[0] = min(max(present_state[0] - 1 - wind_strength, 0), num_rows-1)
+		next_state[1] = min(max(present_state[1], 0), num_columns-1)
+
+	# down
 	elif action == 1:
 		next_state[0] = min(max(present_state[0] + 1 - wind_strength, 0), num_rows-1)
-		next_state[1] = max(present_state[1], 0)
+		next_state[1] = min(max(present_state[1], 0), num_columns-1)
+
+	# right
 	elif action == 2:
-		next_state[0] = max(present_state[0] - wind_strength, 0)
-		next_state[1] = min(present_state[1] + 1, num_columns-1)
+		next_state[0] = min(max(present_state[0] - wind_strength, 0), num_rows-1)
+		next_state[1] = min(max(present_state[1] + 1, 0), num_columns-1)
+	
+	# left
 	elif action == 3:
-		next_state[0] = max(present_state[0] - wind_strength, 0)
-		next_state[1] = max(present_state[1] - 1, 0)
+		next_state[0] = min(max(present_state[0] - wind_strength, 0), num_rows-1)
+		next_state[1] = min(max(present_state[1] - 1, 0), num_columns-1)
+
+	# north-east
+	elif action == 4: 
+		next_state[0] = min(max(present_state[0] - 1 - wind_strength, 0), num_rows-1)
+		next_state[1] = min(max(present_state[1] + 1, 0), num_columns-1)
+
+	# north-west
+	elif action == 5:
+		next_state[0] = min(max(present_state[0] - 1 - wind_strength, 0), num_rows-1)
+		next_state[1] = min(max(present_state[1] - 1, 0), num_columns-1)
+
+	# south-east
+	elif action == 6:
+		next_state[0] = min(max(present_state[0] + 1 - wind_strength, 0), num_rows-1)
+		next_state[1] = min(max(present_state[1] + 1, 0), num_columns-1)
+
+	# south-west
+	elif action == 7:
+		next_state[0] = min(max(present_state[0] + 1 - wind_strength, 0), num_rows-1)
+		next_state[1] = min(max(present_state[1] - 1, 0), num_columns-1)
 
 	if next_state[0] == goal_block[0] and next_state[1] == goal_block[1]:
 		reward = 0
 
 	return next_state, reward
 
-def epsilon_greedy(present_state, epsilon, num_actions, Q):
+def epsilon_greedy(present_state, epsilon, num_actions, Q, seed):
+	# np.random.seed(seed)
 	random1 = np.random.rand(1, 1)
 	action = 0
 	if random1 < epsilon: 
@@ -41,20 +68,22 @@ def epsilon_greedy(present_state, epsilon, num_actions, Q):
 
 	return action
 
-def sarsa_on_policy(num_rows, num_columns, num_actions, num_episodes, wind_dist, start_block, goal_block, alpha, epsilon, num_runs, gamma):
+def sarsa_on_policy(num_rows, num_columns, num_actions, num_episodes, wind_dist, start_block, goal_block, alpha, epsilon, num_runs, gamma, stochastic):
 	Q = np.zeros((num_rows, num_columns, num_actions), dtype=float)
 	episodes_steps = np.zeros(num_episodes, dtype=float)
 
 	for n in range(0, num_runs):
 		print (n)
+		seed = 2*n
+		# np.random.seed(seed)
 		for x in range(0,num_episodes):
 			present_state = start_block
-			action = epsilon_greedy(present_state, epsilon, num_actions, Q)
+			action = epsilon_greedy(present_state, epsilon, num_actions, Q, seed)
 			time_steps = 0
 
 			while(1):
-				state_dash, reward = nextstate_reward(num_rows, num_columns, wind_dist, start_block, goal_block, present_state, action)
-				action_dash = epsilon_greedy(state_dash, epsilon, num_actions, Q)
+				state_dash, reward = nextstate_reward(num_rows, num_columns, wind_dist, start_block, goal_block, present_state, action, stochastic, seed)
+				action_dash = epsilon_greedy(state_dash, epsilon, num_actions, Q, seed)
 				Q[present_state[0]][present_state[1]][action] += alpha*(reward + gamma*Q[state_dash[0]][state_dash[1]][action_dash] - Q[present_state[0]][present_state[1]][action])
 				present_state = state_dash
 				action = action_dash
@@ -75,23 +104,40 @@ if __name__ == '__main__':
 	alpha = 0.5
 	epsilon = 0.1
 	num_episodes = 170
-	num_runs = 30
+	num_runs = 10
 	num_actions = 4
 	gamma = 1
+	stochastic = 0
 	wind_dist = np.asarray([0, 0, 0, 1, 1, 1, 2, 2, 1, 0], dtype=int)
 	start_block = np.asarray([3, 0], dtype=int)
 	goal_block = np.asarray([3, 7], dtype=int)
-	episodes_steps = sarsa_on_policy(num_rows, num_columns, num_actions, num_episodes, wind_dist, start_block, goal_block, alpha, epsilon, num_runs, gamma)
-	episodes = [i for i in range(num_episodes)]
-	times = np.zeros(len(episodes_steps)+1)
-	times[1:] = episodes_steps
-	y = np.zeros(len(episodes)+1)
-	y[1:] = episodes
+	episodes_steps1 = sarsa_on_policy(num_rows, num_columns, num_actions, num_episodes, wind_dist, start_block, goal_block, alpha, epsilon, num_runs, gamma, stochastic)
+	episodes1 = [i for i in range(num_episodes)]
+	times1 = np.zeros(len(episodes_steps1)+1)
+	times1[1:] = episodes_steps1
+	y1 = np.zeros(len(episodes1)+1)
+	y1[1:] = episodes1
 
-	plt.plot(times, y)
+	num_actions = 8
+	episodes_steps2 = sarsa_on_policy(num_rows, num_columns, num_actions, num_episodes, wind_dist, start_block, goal_block, alpha, epsilon, num_runs, gamma, stochastic)
+	episodes2 = [i for i in range(num_episodes)]
+	times2 = np.zeros(len(episodes_steps2)+1)
+	times2[1:] = episodes_steps2
+	y2 = np.zeros(len(episodes2)+1)
+	y2[1:] = episodes2
+
+	stochastic = 1
+	episodes_steps3 = sarsa_on_policy(num_rows, num_columns, num_actions, num_episodes, wind_dist, start_block, goal_block, alpha, epsilon, num_runs, gamma, stochastic)
+	episodes3 = [i for i in range(num_episodes)]
+	times3 = np.zeros(len(episodes_steps3)+1)
+	times3[1:] = episodes_steps3
+	y3 = np.zeros(len(episodes3)+1)
+	y3[1:] = episodes3
+
+	plt.plot(times1, y1, label="Normal")
+	plt.plot(times2, y2, label="King's Moves")
+	plt.plot(times3, y3, label="King's Moves with Stochasticity")
+	plt.legend(loc='upper left')
+	plt.xlabel("Time Steps")
+	plt.ylabel("Episodes")
 	plt.show()
-
-
-
-
-
